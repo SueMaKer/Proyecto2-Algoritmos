86805aceee7fab3b1a7c45b3acbbfcc6ddcf1f34
Melanny Hernández <melanny.hernandez@ucr.ac.cr>
2025-05-19 22:23:50 -0600

    Implementando las bases de las clases del juego

---
+++ Player.cpp
@@ -0,0 +1,30 @@
+#include "Player.hpp"
+
+using namespace std;
+
+Player::Player(const std::string& initialName) {
+    //se inicializara el nombre del jugador	
+}
+
+int Player::getEtherium() const {
+    // Se obtiene la cantidad de etherium del jugador
+    return 0;
+}
+
+void Player::addEtherium(int amount) {
+    // Agrega etherium al jugador
+}
+
+bool Player::spendEtherium(int amount) {
+    // Gasta etherium del jugador
+    return false;
+}
+
+std::string Player::getName() const {
+    // Retornará el nombre del jugador
+    return "";
+}
+
+void Player::displayStatus() const {
+    // muestra el estado del jugador
+}
---
+++ Player.hpp
@@ -0,0 +1,31 @@
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+#include <vector>
+#include <string>
+#include <memory>
+
+class Player {
+private:
+    std::string name; // Player's name
+    int etherium; // Amount of etherium per player
+    //std::vector<std::unique_ptr<Ship>> fleet; // Player's fleet of ships
+
+public:
+    Player(const std::string& initialName);
+
+    // Resources
+    int getEtherium() const;
+    void addEtherium(int amount);
+    bool spendEtherium(int amount);
+
+    // Actions
+    
+
+
+    // Info
+    std::string getName() const;
+    void displayStatus() const;
+};
+
+#endif // PLAYER_HPP
