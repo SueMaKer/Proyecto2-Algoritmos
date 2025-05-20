7d769e780df86e88c2d227011706f55559b51ee1
Melanny Hernández <melanny.hernandez@ucr.ac.cr>
2025-05-19 23:09:49 -0600

    iniciando la implementación de la clase Game.

---
+++ Game.cpp
@@ -0,0 +1,18 @@
+#include "Game.hpp"
+#include <iostream>
+
+Game::Game() {
+turn = 1; //Es un 1 porque es el primer turno
+}
+
+void Game::start() {
+//Inicialización del juego
+}
+
+void Game::nextTurn() {
+// Lógica para avanzar al siguiente turno
+}
+
+void Game::showStatus() {
+// Mostrar el estado actual del juego
+}
---
+++ Game.hpp
@@ -0,0 +1,15 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+class Game {
+private:
+    int turn;
+
+public:
+    Game(); // Constructor
+    void start(); 
+    void nextTurn();
+    void showStatus();
+};
+
+#endif // GAME_HPP
